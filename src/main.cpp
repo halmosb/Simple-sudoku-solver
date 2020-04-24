8c2ce814519660a1330772b560d5961a22e5c34e
Halmos Balázs <halmosbalazsp@gmail.com>
2020-04-24 14:07:05 +0200

    Adding the function that prints the table to the standard output

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void printTable(vector<vector<char>> &tabl);
+
 int main(){
-    cout << "Hello World!\n\n";
+    
+    vector<vector<char>> sudoku = {{'1','2','3'}, {'4','5','6'}, {'7','8','9'}};
+    
+    printTable(sudoku);
+
     return 0;
+}
+
+// This is the function that prints the table of the sudoku game to the standard output
+void printTable(vector<vector<char>> &tabl){
+
+    for(int i = 0; i < tabl.size(); i++){
+        for(int j = 0; j < tabl[0].size(); j++){
+            cout << tabl[i][j];
+        }
+        cout<<endl;
+    }
+
 }
